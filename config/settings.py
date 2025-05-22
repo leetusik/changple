03c815afdeb4bb03486cf7f7e89c6582a89dad85
leetusik <leetusik@gmail.com>
2025-05-23 01:30:25 +0900

    hello

--- config/settings.py
+++ config/settings.py
@@ -310,3 +310,5 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Your App <noreply@yourapp.com>")
+
+# heeloo
